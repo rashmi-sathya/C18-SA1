16766b20b5cdffce0694817fa4dce397e4f151e0
rashmi-sathya <72431927+rashmi-sathya@users.noreply.github.com>
2021-05-15 16:32:03 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,10 @@
+// Initialize a global variable
+var variablename = "global variable";
+
+function transform() {
+  // Initialize a local, function-scoped variable
+  var variablename = "local variable";
+  console.log(variablename);
+}
+
+// Log the global and local variable
